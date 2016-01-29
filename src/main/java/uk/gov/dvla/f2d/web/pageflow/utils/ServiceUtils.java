deb0ad23bb84881e7c6267160f7560fd6cd383bc
James Bishop <james.bishop@digital.dvla.gov.uk>
2016-01-29 13:47:52 +0000

    Added unit tests for services and language support.

--- src/main/java/uk/gov/dvla/f2d/web/pageflow/utils/ServiceUtils.java
+++ src/main/java/uk/gov/dvla/f2d/web/pageflow/utils/ServiceUtils.java
@@ -2,6 +2,7 @@ package uk.gov.dvla.f2d.web.pageflow.utils;
 
 import java.util.Arrays;
 
+import static uk.gov.dvla.f2d.web.pageflow.constants.Constants.SUPPORTED_LANGUAGES;
 import static uk.gov.dvla.f2d.web.pageflow.constants.Constants.SUPPORTED_SERVICES;
 
 public class ServiceUtils
@@ -13,6 +14,12 @@ public class ServiceUtils
     public static void checkServiceSupported(final String service) {
         if(!(Arrays.asList(SUPPORTED_SERVICES).contains(service))) {
             throw new IllegalArgumentException("Service is not currently supported: " + service);
+        }
+    }
+
+    public static void checkLanguageSupported(final String language) {
+        if(!(Arrays.asList(SUPPORTED_LANGUAGES).contains(language))) {
+            throw new IllegalArgumentException("Language is not currently supported: " + language);
         }
     }
 }
---
+++ src/test/java/uk/gov/dvla/f2d/web/pageflow/utils/ServiceUtilsTest.java
@@ -0,0 +1,74 @@
+package uk.gov.dvla.f2d.web.pageflow.utils;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import static uk.gov.dvla.f2d.web.pageflow.constants.Constants.*;
+
+public class ServiceUtilsTest extends TestCase
+{
+    /**
+     * Create the test case
+     * @param testName name of the test case
+     */
+    public ServiceUtilsTest(String testName ) {
+        super(testName);
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite() {
+        return new TestSuite(ServiceUtilsTest.class);
+    }
+
+    public void testNotifyServiceSupported() {
+        try {
+            ServiceUtils.checkServiceSupported(NOTIFY_SERVICE);
+
+        } catch(IllegalArgumentException ex) {
+            fail("An IllegalArgumentException should not have been raised.");
+        }
+    }
+
+    public void testRenewalServiceUnsupported() {
+        try {
+            ServiceUtils.checkServiceSupported("renewal");
+
+            fail("An IllegalArgumentException should have been raised.");
+
+        } catch(IllegalArgumentException ex) {
+            // This is the expected behaviour.
+        }
+    }
+
+    public void testEnglishLanguageSupported() {
+        try {
+            ServiceUtils.checkLanguageSupported(ENGLISH_LANGUAGE);
+
+        } catch(IllegalArgumentException ex) {
+            fail("An IllegalArgumentException should not have been raised.");
+        }
+    }
+
+    public void testWelshLanguageSupported() {
+        try {
+            ServiceUtils.checkLanguageSupported(WELSH_LANGUAGE);
+
+        } catch(IllegalArgumentException ex) {
+            fail("An IllegalArgumentException should not have been raised.");
+        }
+    }
+
+    public void testSpanishLanguageUnsupported() {
+        try {
+            ServiceUtils.checkLanguageSupported("es");
+
+            fail("An IllegalArgumentException should not have been raised.");
+
+        } catch(IllegalArgumentException ex) {
+            // This is the expected behaviour.
+        }
+    }
+}
