7cf33ef89b908ae9f2ff1e97b9241ca7bf7b27b9
James Bishop <james.bishop@digital.dvla.gov.uk>
2016-02-01 14:47:53 +0000

    Configuration changes for special adaptations.

--- src/main/java/uk/gov/dvla/f2d/web/pageflow/constants/Constants.java
+++ src/main/java/uk/gov/dvla/f2d/web/pageflow/constants/Constants.java
@@ -43,4 +43,5 @@ public class Constants
     public static final String COMMA_SYMBOL        = ",";
     public static final String EQUALS_SYMBOL       = "=";
     public static final String HYPHEN_SYMBOL       = "-";
+    public static final String PERIOD_SYMBOL       = ".";
 }
