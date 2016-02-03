9b0b14b5d01ca5f987cb49439eef2520e6025dd2
James Bishop <james.bishop@digital.dvla.gov.uk>
2016-02-03 12:19:11 +0000

    Bug fixes for checkboxes.

--- src/main/java/uk/gov/dvla/f2d/web/pageflow/processor/impl/DataProcessorFactory.java
+++ src/main/java/uk/gov/dvla/f2d/web/pageflow/processor/impl/DataProcessorFactory.java
@@ -3,6 +3,7 @@ package uk.gov.dvla.f2d.web.pageflow.processor.impl;
 import uk.gov.dvla.f2d.web.pageflow.enums.Format;
 import uk.gov.dvla.f2d.web.pageflow.model.MedicalQuestion;
 import uk.gov.dvla.f2d.web.pageflow.model.MessageHeader;
+import uk.gov.dvla.f2d.web.pageflow.utils.LogUtils;
 
 public final class DataProcessorFactory
 {
@@ -13,16 +14,22 @@ public final class DataProcessorFactory
     public IDataQuestionProcessor getQuestionProcessor(MedicalQuestion question) {
         IDataQuestionProcessor processor = null;
 
+        LogUtils.debug(this.getClass(), "Question Type: " + question.getType());
+
         if(Format.FORM.equals(question.getType())) {
+            LogUtils.debug(this.getClass(), "  + Form Processor");
             processor = new DataProcessorFormPageImpl(question);
 
         } else if(Format.RADIO.equals(question.getType())) {
+            LogUtils.debug(this.getClass(), "  + Radio Processor");
             processor = new DataProcessorRadioGroupImpl(question);
 
         } else if(Format.CHECKBOX.equals(question.getType())) {
+            LogUtils.debug(this.getClass(), "  + CheckBox Processor");
             processor = new DataProcessorCheckboxGroupImpl(question);
 
         } else if(Format.CONTINUE.equals(question.getType())) {
+            LogUtils.debug(this.getClass(), "  + Continue Processor");
             processor = new DataProcessorContinuePageImpl(question);
 
         } else {
