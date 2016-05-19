270f4a9df1a2baf16dc840c3515d589a827ec02e
James Bishop <james.bishop@digital.dvla.gov.uk>
2016-05-19 13:27:26 +0100

    Updated business model version.

--- src/test/java/uk/gov/dvla/f2d/web/pageflow/utils/MapUtilsTest.java
+++ src/test/java/uk/gov/dvla/f2d/web/pageflow/utils/MapUtilsTest.java
@@ -1,12 +1,16 @@
 package uk.gov.dvla.f2d.web.pageflow.utils;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import uk.gov.dvla.f2d.model.enums.Language;
 import uk.gov.dvla.f2d.model.enums.Service;
 import uk.gov.dvla.f2d.model.pageflow.MedicalCondition;
+import uk.gov.dvla.f2d.model.pageflow.MedicalForm;
 import uk.gov.dvla.f2d.web.pageflow.config.PageFlowCacheManager;
 
+import java.io.IOException;
 import java.util.Map;
 
 public class MapUtilsTest extends TestCase
@@ -35,5 +39,45 @@ public class MapUtilsTest extends TestCase
 
         assertTrue("No conditions were found in data structure", conditions.size() > 0);
         assertTrue("No questions were found in data structure", conditions.size() > 0);
+    }
+
+    /**
+     * Test to see if our medical structure can be transformed into raw data.
+     */
+    public void testMapMedicalFormToDataString() {
+        try {
+            MedicalForm form = PageFlowCacheManager.getMedicalForm(Service.NOTIFY);
+            String data = MapUtils.mapModelToString(form);
+
+            assertNotNull(data);
+            assertTrue(data.trim().length() > 0);
+
+        } catch(JsonProcessingException ex) {
+            fail("A JsonProcessingException should not have been raised!");
+        }
+    }
+
+    /**
+     * Test to see if our medical structure can be transformed into raw data.
+     */
+    public void testMapDataStringToMedicalForm() {
+        try {
+            final String data = "{\"messageHeader\":{\"service\":\"notify\",\"language\":\"en\",\"notifications\":[],\"breadcrumb\":[],\"authentication\":{\"verified\":false}},\"personalDetails\":null,\"medicalCondition\":null}\n";
+            assertNotNull(data);
+
+            MedicalForm form = MapUtils.mapStringToModel(data);
+            assertNotNull(form);
+
+            assertEquals(form.getMessageHeader().getService(), Service.NOTIFY.toString());
+            assertEquals(form.getMessageHeader().getLanguage(), Language.ENGLISH.toString());
+            assertTrue(form.getMessageHeader().getBreadcrumb().isEmpty());
+            assertTrue(form.getMessageHeader().getNotifications().isEmpty());
+
+        } catch(JsonProcessingException ex) {
+            fail("A JsonProcessingException should not have been raised!");
+
+        } catch(IOException ex) {
+            fail("A IOException should not have been raised!");
+        }
     }
 }
