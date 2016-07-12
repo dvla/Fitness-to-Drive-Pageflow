60da9a047c84f8c17474dd45129c94d08511adf8
James Bishop <james.bishop@digital.dvla.gov.uk>
2016-07-12 11:09:14 +0100

    Fixed casting problems  with component mapper.

--- src/main/java/uk/gov/dvla/f2d/web/pageflow/processor/components/config/ControllerComponentConfiguration.java
+++ src/main/java/uk/gov/dvla/f2d/web/pageflow/processor/components/config/ControllerComponentConfiguration.java
@@ -1,11 +1,12 @@
 package uk.gov.dvla.f2d.web.pageflow.processor.components.config;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import uk.gov.dvla.f2d.web.pageflow.processor.mappers.config.ControllerComponentMapper;
 import uk.gov.dvla.f2d.web.pageflow.processor.mappers.config.RadioComponentMapper;
 
 import java.util.Map;
 
-@JsonDeserialize(using = RadioComponentMapper.class)
+@JsonDeserialize(using = ControllerComponentMapper.class)
 public final class ControllerComponentConfiguration
 {
     private Map<String, String> options;
