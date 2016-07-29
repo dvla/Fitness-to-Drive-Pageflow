a239299abe5524d76f1917c248fc9e82f3a6108a
Leszek Gurniewicz <Leszek.Gurniewicz@dvla.gsi.gov.uk>
2016-07-29 10:55:27 +0100

    692 - Clinic details on consultant page
    * Update ConsultantDetails validation to take into account 2 fields
    * Override AbstractFormValidator checkMethod to use custom validation
    * Add new error code
    * Add mockito to pom.xml
    * create test for new validation

--- src/main/java/uk/gov/dvla/f2d/web/pageflow/constants/ErrorCodes.java
+++ src/main/java/uk/gov/dvla/f2d/web/pageflow/constants/ErrorCodes.java
@@ -8,6 +8,7 @@ public final class ErrorCodes
 
     public static final String NULL_OR_EMPTY_CODE           = "NullOrEmpty";
     public static final String NULL_OR_EMPTY_DESC           = "Mandatory field supplied was empty.";
+    public static final String AT_LEAST_ONE_FIELD           = "AtLeastOneField";
 
     public static final String INVALID_OPTION_CODE          = "InvalidOption";
     public static final String INVALID_OPTION_DESC          = "Mandatory field supplied was invalid.";
--- src/main/java/uk/gov/dvla/f2d/web/pageflow/processor/validation/forms/ConsultantDetails.java
+++ src/main/java/uk/gov/dvla/f2d/web/pageflow/processor/validation/forms/ConsultantDetails.java
@@ -2,24 +2,80 @@ package uk.gov.dvla.f2d.web.pageflow.processor.validation.forms;
 
 import uk.gov.dvla.f2d.model.pageflow.MedicalForm;
 import uk.gov.dvla.f2d.model.pageflow.MedicalQuestion;
+import uk.gov.dvla.f2d.model.pageflow.Notification;
+import uk.gov.dvla.f2d.model.utils.StringUtils;
+import uk.gov.dvla.f2d.web.pageflow.forms.PageForm;
+import uk.gov.dvla.f2d.web.pageflow.processor.validation.IFormValidator;
+import uk.gov.dvla.f2d.web.pageflow.utils.PageFlowUtils;
 
-public class ConsultantDetails extends AbstractFormValidator
+import java.util.ArrayList;
+import java.util.List;
+
+import static uk.gov.dvla.f2d.web.pageflow.constants.ErrorCodes.AT_LEAST_ONE_FIELD;
+import static uk.gov.dvla.f2d.web.pageflow.constants.ErrorCodes.NULL_OR_EMPTY_CODE;
+import static uk.gov.dvla.f2d.web.pageflow.constants.ErrorCodes.NULL_OR_EMPTY_DESC;
+
+public class ConsultantDetails extends AbstractFormValidator implements IFormValidator
 {
-    private static final String CONSULTANT_NAME     = "consultantName";
-    private static final String HOSPITAL_NAME       = "hospitalName";
-    private static final String POST_TOWN           = "postTown";
-    private static final String POST_CODE           = "postCode";
-    private static final String PHONE_NUMBER        = "phoneNumber";
+    static final String CONSULTANT_NAME     = "consultantName";
+    static final String CLINIC_NAME         = "clinicName";
+    static final String HOSPITAL_NAME       = "hospitalName";
+    static final String POST_TOWN           = "postTown";
+    static final String POST_CODE           = "postCode";
+    static final String PHONE_NUMBER        = "phoneNumber";
+
+    private static final String[] PERSISTED_FIELDS = {
+            CONSULTANT_NAME, CLINIC_NAME, HOSPITAL_NAME, POST_TOWN, POST_CODE, PHONE_NUMBER
+    };
+
+    private static final String[] MANDATORY_FIELDS = {
+            CONSULTANT_NAME, CLINIC_NAME, HOSPITAL_NAME, POST_TOWN
+    };
 
     public ConsultantDetails(MedicalForm form, MedicalQuestion question) {
         super(form, question);
     }
 
-    String[] getPersistentFields() {
-        return new String[] { CONSULTANT_NAME, HOSPITAL_NAME, POST_TOWN, POST_CODE, PHONE_NUMBER};
+    @Override
+    public String[] getPersistentFields() {
+        return PERSISTED_FIELDS;
     }
 
-    String[] getMandatoryFields() {
-        return new String[] { CONSULTANT_NAME, HOSPITAL_NAME, POST_TOWN};
+    @Override
+    public String[] getMandatoryFields() {
+        return MANDATORY_FIELDS;
+    }
+
+    @Override
+    List<Notification> checkMandatoryFieldsSupplied(PageForm pageForm) {
+        return execute(pageForm);
+    }
+
+    public List<Notification> execute(PageForm pageForm) {
+
+        List<Notification> codes = new ArrayList<>();
+        if(StringUtils.isNullOrEmpty(getFormField(pageForm, POST_TOWN))) {
+            codes.add(createNotification(POST_TOWN, NULL_OR_EMPTY_CODE, NULL_OR_EMPTY_DESC));
+        }
+        if(StringUtils.isNullOrEmpty(getFormField(pageForm, CONSULTANT_NAME))) {
+            codes.add(createNotification(CONSULTANT_NAME, NULL_OR_EMPTY_CODE, NULL_OR_EMPTY_DESC));
+        }
+        if(StringUtils.isNullOrEmpty(getFormField(pageForm, CLINIC_NAME)) &&
+                StringUtils.isNullOrEmpty(getFormField(pageForm, HOSPITAL_NAME)) ) {
+            codes.add(createNotification(CLINIC_NAME, AT_LEAST_ONE_FIELD, ""));
+            codes.add(createNotification(HOSPITAL_NAME, AT_LEAST_ONE_FIELD, ""));
+        }
+
+        return codes;
+    }
+
+    private Notification createNotification(String field, String code, String description) {
+        Notification notification = new Notification();
+        notification.setPage(PageFlowUtils.capitalise(getQuestion()));
+        notification.setField(field);
+        notification.setCode(code);
+        notification.setDescription(description);
+        return notification;
     }
 }
+
